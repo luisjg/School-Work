51d65042f8a0e2e6ac51cbc730826ed6a4d7b05d
luisjg <luisjguzman87@gmail.com>
2015-10-11 22:50:30 -0700

    Close to final
    Algorithm works for problem 4a
    
    TODO
    - Need to check if it works for a general problem

--- 496/extra-credit/ExtraCreditProblem.java
+++ 496/extra-credit/ExtraCreditProblem.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+
 class ExtraCreditProblem{
   public static void main(String[] args){
-    System.out.println("ExtreCreditProblem");
+    // problem 4a
+    int size=5;
+    char[] label ={'a','b','c','d','e'};
+    ArrayList<Character> sort=new ArrayList<>();
+    int[][] graph={{0,0,0,1,0},
+                   {1,0,1,0,0},
+                   {0,0,0,0,0},
+                   {0,0,1,0,0},
+                   {1,0,0,1,0}};
+
+    // look for columns that have all 0's
+    int i=0;
+    int count=0;
+    while(i<size){
+      for(int k=0; k<size; k++){
+        if(graph[k][i] == 0){
+          count++;
+        }
+        if(count == size){
+          for(int j=0;j<size;j++){
+            if(graph[i][j] == 1)
+              graph[i][j] = 0;
+            graph[j][i]=-1;
+          }
+          sort.add(label[i]);
+          i=-1;
+        }
+      }
+      count=0;
+      i++;
+    }
+
+    for(int k=0;k<sort.size();k++)
+      System.out.println(sort.get(k));
+  }
+
+  public static void printArray(int[][] print,int s){
+    for(int i=0; i<s; i++){
+      for(int k=0; k<s; k++)
+        System.out.print(print[i][k] + " ");
+    System.out.println();
+    }
+  }
+
+  public static void printMenu(){
+
   }
 }

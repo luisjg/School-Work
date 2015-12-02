1052578c379c8ff05e06467698fe4c2f14aa0f02
luisjg <luisjguzman87@gmail.com>
2015-12-01 22:01:55 -0800

    Initial Commit
    TODO find subset sum

---
+++ 496/project3/KnapsackProblem.java
@@ -0,0 +1,9 @@
+public class KnapsackProblem{
+  int n; // size of the problem
+  int[] weight; // weight of items
+  int[] value; // value of items
+  int W; // weight capacity
+  public static void main(String[] args){
+
+  }
+}
